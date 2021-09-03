762333c85a19d47175fc09119e39b21cdf56317c
Mohammad Talham <mrtalhim@gmail.com>
2021-09-03 23:02:33 +0700

    fix substitution cipher with dict.fromkeys

--- cipher_function/substitution_cipher/substitution_cipher.py
+++ cipher_function/substitution_cipher/substitution_cipher.py
@@ -3,7 +3,7 @@ from cipher_function import helper_function as hf
 def encrypt(key, plaintext, cipher_split=False):
     plain_alphabet = hf.alphabet_init()
     plaintext = hf.plaintext_prep(plaintext)
-    cipher_alphabet = {x for x in key}
+    cipher_alphabet = dict.fromkeys(x for x in key)
     cipher_alphabet = list(cipher_alphabet)
 
     for x in plain_alphabet:
@@ -21,7 +21,7 @@ def encrypt(key, plaintext, cipher_split=False):
 def decrypt(key, ciphertext):
     plain_alphabet = hf.alphabet_init()
     ciphertext = hf.plaintext_prep(ciphertext)
-    cipher_alphabet = {x for x in key}
+    cipher_alphabet = dict.fromkeys(x for x in key)
     cipher_alphabet = list(cipher_alphabet)
     
     for x in plain_alphabet:
