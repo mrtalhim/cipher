550042b50c813d5e6ed01eff67eeaa9e6601d502
Mohammad Talham <mrtalhim@gmail.com>
2021-09-03 13:32:18 +0700

    added shift cipher decrypt function

--- cipher_function/shift_cipher.py
+++ cipher_function/shift_cipher.py
@@ -2,7 +2,35 @@ import helper_function as hf
 
 def shift_cipher_encrypt(key, plaintext, cipher_split=False):
     plain_alphabet = hf.alphabet_init()
-    cipher_alphabet = [plain_alphabet[(plain_alphabet.index(x) + key) % 26] for x in plain_alphabet]
+    cipher_alphabet = [
+        plain_alphabet[(plain_alphabet.index(x) + key) % 26]
+        for x in plain_alphabet
+    ]
     plaintext = hf.plaintext_prep(plaintext)
-    ciphertext = ''.join([cipher_alphabet[plain_alphabet.index(x)] if x.isalpha() else x for x in plaintext])
+    ciphertext = ''.join([
+        cipher_alphabet[plain_alphabet.index(x)]
+        if x.isalpha() else x
+        for x in plaintext
+    ])
     return hf.present_ciphertext(ciphertext, split=cipher_split)
+
+def shift_cipher_decrypt(key, ciphertext):
+    plain_alphabet = hf.alphabet_init()
+    cipher_alphabet = [
+        plain_alphabet[(plain_alphabet.index(x) - key) % 26]
+        for x in plain_alphabet
+    ]
+    ciphertext = hf.plaintext_prep(ciphertext)
+    plaintext = ''.join([
+        cipher_alphabet[plain_alphabet.index(x)]
+        if x.isalpha() else x
+        for x in ciphertext
+    ])
+    return hf.present_ciphertext(plaintext)
+
+key = 3
+plaintext = 'hello world'
+ciphertext = shift_cipher_encrypt(key, plaintext)
+deciphered = shift_cipher_decrypt(key, ciphertext)
+print(ciphertext)
+print(deciphered)
