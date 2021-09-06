b4bd40ef539857d15b68bfae091953681dc2e1c0
Mohammad Talham <mrtalhim@gmail.com>
2021-09-07 06:18:49 +0700

    code cleanup for standart_vigenere_cipher

--- standart_vigenere_cipher.py
+++ standart_vigenere_cipher.py
@@ -22,13 +22,16 @@ def standart_vigenere_cipher(key, input, mode='encrypt', cipher_split=False):
 def encrypt(key, plaintext, cipher_split=False):
     plain_alphabet = hf.alphabet_init()
     plaintext = hf.plaintext_prep(plaintext, alpha_only=True)
+    
     while len(key) <= len(plaintext):
         key += key
     ciphertext = ''
+    
     for x in range(len(plaintext)):
         cipher = hf.alphabet_index(plaintext[x], plain_alphabet) + hf.alphabet_index(key[x], plain_alphabet)
         cipher = cipher % len(plain_alphabet)
         ciphertext += plain_alphabet[cipher]
+        
     return hf.present_ciphertext(ciphertext, split=cipher_split)
 
 def decrypt(key, ciphertext):
