d2c8521e05455ca49446016a2c2210d1171fa093
Mohammad Talham <mrtalhim@gmail.com>
2021-09-09 08:58:07 +0700

    fixed input and return value to binary

--- extended_vigenere_cipher.py
+++ extended_vigenere_cipher.py
@@ -1,23 +1,24 @@
 import helper_function as hf
-import struct
 
 def extended_vigenere_cipher(key, input, mode='encrypt', cipher_split=False):
     """Python implementation of Vigenere Cipher for binary file input with ASCII key
 
     Args:
         key (str): Key for cipher operation.
-        input (str): Text to encrypt/decrypt
+        input (str, binary): Text to encrypt/decrypt
         mode (str, optional): Selects mode to do between 'encrypt' or 'decrypt'. Defaults to 'encrypt'.
         cipher_split (bool, optional): Selects cipher encryption output between no space if False and 5-character group if True. Defaults to False.
 
     Returns:
         str: text output from cipher operation
     """
+    input = input.decode()
+    
     if mode=='encrypt':
         return encrypt(key, input, cipher_split=cipher_split)
     
-    elif mode=='decypt':
-        return None
+    elif mode=='decrypt':
+        return decrypt(key, input)
 
 def encrypt(key, plaintext, cipher_split=False):
     plain_alphabet = [chr(x) for x in range(256)]
@@ -31,11 +32,10 @@ def encrypt(key, plaintext, cipher_split=False):
         cipher = cipher % len(plain_alphabet)
         ciphertext += plain_alphabet[cipher]
     
-    return ciphertext
+    return ciphertext.encode()
 
 def decrypt(key, ciphertext):
     plain_alphabet = [chr(x) for x in range(256)]
-    ciphertext = hf.plaintext_prep(ciphertext)
     
     while len(key) <= len(ciphertext):
         key += key
@@ -45,8 +45,8 @@ def decrypt(key, ciphertext):
         plain = hf.alphabet_index(ciphertext[x], plain_alphabet) - hf.alphabet_index(key[x], plain_alphabet)
         plain = plain % len(plain_alphabet)
         plaintext += plain_alphabet[plain]
-        
-    return hf.present_ciphertext(plaintext)
+    
+    return plaintext.encode()
 
 if __name__ == "__main__":
     extended_vigenere_cipher()
